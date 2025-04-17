23e8a3f6a2da41ae3f927e3d31b7e8490d66e7fa
LeZi9916 <lezi@leziblog.com>
2025-04-17 18:40:52 +0800

    misc: remove unnecessary files

--- HidSharp.Test/Properties/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-
-[assembly: ComVisible(false)]
-[assembly: Guid("2F0DC374-5974-49E6-A7C6-5FFDEDE5E21D")]
